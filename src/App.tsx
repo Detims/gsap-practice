6ab20845cde53998e4024e58e3ca11f795ed1c54
Detims <2005.nnhan@gmail.com>
2025-09-29 21:52:58 -0700

    gsap fromto

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import theresa from "./assets/theresa1.png"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  useGSAP(() => {
+    gsap.fromTo('.theresa', {
+      x: -250,
+      rotation: 0,
+    },
+    {
+      x: 250,
+      repeat: -1,
+      yoyo: true,
+      rotation: 360,
+      duration: 2,
+      ease: 'power1.inOut',
+    })
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>GSAP to/from/fromTo</h1>
+      <img src={theresa} className='logo theresa' alt='theresa' />
     </>
   )
 }
 
-export default App
+export default App  
